2084a930eff1ccee4cbd0481b25681507586853c
nutria2 <nutriaz@libero.it>
2026-03-21 23:15:40 +0100

    Add files via upload

--- addon.js
+++ addon.js
@@ -18,7 +18,7 @@ const cheerio = require('cheerio');
 const NodeCache = require('node-cache');
 const sharp = require('sharp');
 
-const ADDON_VERSION = '3.0.9';
+const ADDON_VERSION = '3.0.6';
 
 const PORT = process.env.PORT || 7000;
 const BASE_URL = process.env.BASE_URL || '';
@@ -773,15 +773,6 @@ function sendText(res, statusCode, text) {
   res.end(text);
 }
 
-function sendRedirect(res, location, statusCode = 302) {
-  res.writeHead(statusCode, {
-    'Location': location,
-    'Access-Control-Allow-Origin': '*'
-  });
-  res.end();
-}
-
-
 function sendStaticFile(res, filePath, contentType) {
   if (!fs.existsSync(filePath)) {
     return sendText(res, 404, 'File non trovato');
@@ -892,26 +883,13 @@ const server = http.createServer(async (req, res) => {
     return sendStaticFile(res, path.join(__dirname, 'davinotti-background.png'), 'image/png');
   }
 
-  if (pathname === '/configure') {
-  return sendRedirect(res, '/configure.html');
-}
-
-if (pathname === '/configure.html') {
-      try {
+  if (pathname === '/configure' || pathname === '/configure.html') {
+    try {
       return sendHtml(res, renderConfigureHtml(reqHost));
     } catch (err) {
       return sendText(res, 500, `Errore caricamento configure.html: ${err.message}`);
     }
   }
-  //Fix per chiamata impostazioni da lista addon di stremio
-	const configConfigureMatch = pathname.match(/^\/([^/]+)\/configure(?:\.html)?$/);
-	if (configConfigureMatch) {
-	  try {
-		return sendRedirect(res, '/configure.html');
-	  } catch (err) {
-		return sendText(res, 500, `Errore caricamento configure.html: ${err.message}`);
-	  }
-	}
 
   if (pathname === '/manifest.json') {
     const router = buildRouterForConfig({ feeds: DEFAULT_FEEDS }, reqHost);
